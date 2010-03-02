6baef0d7e5e0e72ea2d4a01fb251f8104f995e52
jnsnjohan <jnsnjohan@32353942-264d-11df-90c7-81228a7a91d5>
2010-03-02 23:20:50 +0000

    test

--- src/Game.java
+++ src/Game.java
@@ -1,12 +1,15 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class Game extends JPanel implements Runnable {
-	public Game(){
+public class Game extends JPanel implements Runnable
+{
+	public Game()
+	{
 		
 	}
 	
-	public void run() {
+	public void run() 
+	{
 		
 	}
 }
