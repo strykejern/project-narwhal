ab0358dc6f0d5fe5fb4710f92c50152cb1b6804d
jnsn.johan <jnsn.johan@32353942-264d-11df-90c7-81228a7a91d5>
2010-03-05 09:25:57 +0000

    Implemented a hash table to increase memory efficiency when loading previous visited places. Also expanded the Profiler class to become less static and usable in multiple places using a string identifier.

--- trunk/src/gameEngine/Profiler.java
+++ trunk/src/gameEngine/Profiler.java
@@ -1,38 +1,72 @@
 package gameEngine;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Profiler {
 
-	static long startTime = 0;
-	static long endTime = 0;
-	static int numberOfRuns = 0;
-	static float totalRuntime = 0;
-	static boolean profilerActive = false;
+	static Map<String, Profiler> profileList = new HashMap<String, Profiler>();
 	
 	/**
 	 * JJ> Begins monitoring the processing time something uses. Remember to call Profiler.end()
 	 *     when the computation is complete
 	 */
-	public static void begin() {
-		
-		//Already running a profiler
-		if( profilerActive ) return;
-		
-		//Begin profiler
-		startTime = System.currentTimeMillis();
-		numberOfRuns++;
-		
-		//Tell them we have started
-		Log.print("PROFILER: Beginning profiling.");
-		System.out.println("PROFILER: Beginning profiling.");
+	public static void begin( String profileName ) {
+		if( !profileList.containsKey( profileName ) ) profileList.put( profileName,  new Profiler( profileName ) );		
+		profileList.get(profileName).start();
 	}
 
 	/**
 	 * JJ> Ends the profiling and writes out the results.
 	 */
-	public static void end() {
+	public static void end( String profileName ) {
+		if( !profileList.containsKey( profileName ) ) return;		
+		profileList.get(profileName).stop();
+		
+	}
+
+	
+	/*
+	 * JJ> The instanced version of this class can only be accessed through the static functions above
+	 */
+	private long startTime, endTime;
+	private int numberOfRuns;
+	private float totalRuntime;
+	private boolean profilerActive;
+	private String profileName;
+	
+	private Profiler(String newName)
+	{
+		startTime = 0;
+		endTime = 0;
+		numberOfRuns = 0;
+		totalRuntime = 0;
+		profilerActive = false;
+		profileName = newName;		
+	}
+	
+	//Start counting
+	private void start()
+	{
+		//Already running a profiler
+		if( profilerActive ) return;
+		profilerActive = true;
+
+		//Begin profiler
+		startTime = System.currentTimeMillis();
+		numberOfRuns++;
+		
+		//Tell them we have started
+		//We use system console directly to save time
+		System.out.println("PROFILER: " + profileName + " - Beginning profiling.");
+	}
+
+	//Stop counting and spit out feedback
+	private void stop() {
 		
 		//The profiler is not running!
 		if( !profilerActive ) return;
+		profilerActive = false;
 		
 		//Calculate runtime
 		endTime = System.currentTimeMillis()-startTime;
@@ -41,9 +75,8 @@ public class Profiler {
 		totalRuntime += endTime;
 		
 		//Spit out message to tell them the results
-		String output = "PROFILER: Used " + endTime + " milliseconds to generate background. (Average: " + totalRuntime/numberOfRuns + ")";
-		Log.print(output);		
-		System.out.println(output);
+		//We use system console directly to save time
+		System.out.println("PROFILER: " + profileName + " - Used " + endTime + " milliseconds. (Average: " + totalRuntime/numberOfRuns + ")");
 	}
 
 }
