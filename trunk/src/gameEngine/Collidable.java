1f820dec9374645e006f05e192d6118e57b82e13
strykejernene <strykejernene@32353942-264d-11df-90c7-81228a7a91d5>
2010-03-05 23:02:18 +0000

    added collision detection between 2 circles

--- trunk/src/gameEngine/Collidable.java
+++ trunk/src/gameEngine/Collidable.java
@@ -1,31 +1,74 @@
 package gameEngine;
 
 public class Collidable {
-	private int shape;
+	private Shape shape;
 	private float direction;
+	private int width, height;
+	protected Vector pos;
 	
-	static enum SHAPE_TYPE
-	{
-		SHAPE_RECT, 
-		SHAPE_CIRCLE, 
-		SHAPE_TRIANGLE,
-		SHAPE_COUNT;
+	public static enum Shape{
+		RECT,
+		TRIANGLE
 	}
 	
-	public Collidable(int shape){
-		this.shape = shape;
+	/**
+	 * Creates a circle object
+	 * @param size - Radius of the circle
+	 */
+	public Collidable(int size){
+		this.shape = null;
+		width = size;
+		height = size;
 		direction = 0;
+		pos = new Vector();
 	}
 	
+	/**
+	 * Creates a Rectangle or a Triangle object
+	 * @param shape - Type of shape of the object
+	 * @param width - Width of the object
+	 * @param height - Height of the object
+	 */
+	public Collidable(Shape shape, int width, int height){
+		this.shape = shape;
+		this.width = width;
+		this.height = height;
+		direction = 0;
+		pos = new Vector();
+	}
+	
+	/**
+	 * Sets the direction of the object
+	 */
 	protected void setDirection(int degrees){
 		setDirection((float)Math.toRadians(degrees));
 	}
 	
+	/**
+	 * Sets the direction of the object
+	 */
 	protected void setDirection(float radians){
 		this.direction = radians;
 	}
+	
+	private float getRadius(){
+		return (float)width;
+	}
+	
 	// TODO: implement
-	public boolean collidesWith(){
+	public boolean collidesWith(Collidable object){
+		if (this.shape == null)
+		{
+			if (object.shape == null && this.pos.minus(object.pos).length() < this.getRadius() + object.getRadius()) return true;
+		}
+		else if (this.shape == Shape.RECT)
+		{
+			
+		}
+		else if (this.shape == Shape.TRIANGLE)
+		{
+			
+		}
 		return false;
 	}
 }
