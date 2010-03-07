8a38f6baf5be7cf6209e2e4f4db30c3594fa6b5c
jnsn.johan <jnsn.johan@32353942-264d-11df-90c7-81228a7a91d5>
2010-03-07 23:03:58 +0000

    Spaceship now draws again

--- trunk/src/gameEngine/GameObject.java
+++ trunk/src/gameEngine/GameObject.java
@@ -1,5 +1,6 @@
 package gameEngine;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
 public abstract class GameObject extends Physics{
@@ -30,16 +31,34 @@ public abstract class GameObject extends Physics{
 		this.image = image;
 	}
 	
-	public void init(Vector pos, int radius, Image2D image, Keyboard keys){
+	public void init(Vector pos, int radius, Image2D image, Keyboard keys) {
 		super.init(pos, false, radius);
 		this.image = image;
 		this.keys = keys;
 	}
 	
-	public void update(){
+	public void update() {
 		super.update();
 	}
 	
-	public abstract void draw(Graphics g);
-	public abstract void drawCollision(Graphics g);
+	public void draw(Graphics g) {
+		g.drawImage(image.toImage(), pos.getX()-image.getWidth()/2, pos.getY()-image.getHeight()/2, null);		
+	}
+	
+	public void drawCollision(Graphics g) {
+		//Always draw the image bounds
+		int w = image.getWidth();
+		int h = image.getHeight();		
+		g.setColor(Color.BLUE);
+		g.drawRect(pos.getX()-w/2, pos.getY()-h/2, w, h);
+
+		//Draw it as a circle
+		if( super.shape == null )
+		{
+			w = (int)super.getRadius()*2;
+			h = (int)super.getRadius()*2;
+			g.setColor(Color.RED);
+			g.drawOval(pos.getX()-w/2, pos.getY()-h/2, w, h);			
+		}
+	}
 }
