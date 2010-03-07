3c4ae8b17046773eac64628c97add2c41963006e
jnsn.johan <jnsn.johan@32353942-264d-11df-90c7-81228a7a91d5>
2010-03-07 23:19:17 +0000

    Bounding box

--- trunk/src/gameEngine/Spaceship.java
+++ trunk/src/gameEngine/Spaceship.java
@@ -1,9 +1,13 @@
 package gameEngine;
 
+import narwhal.Game;
+
 public class Spaceship extends GameObject{
 	
 	public Spaceship(Vector pos, Image2D image, Keyboard keys){
 		super.init(pos, image.getWidth()/2, image, keys);
+		image.resize(Game.getScreenWidth()/8, Game.getScreenWidth()/8);
+		super.setRadius(image.getWidth()/2 );
 	}
 
 	public void update(){
