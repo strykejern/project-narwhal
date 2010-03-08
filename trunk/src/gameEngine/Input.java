55d15464b499597a99c89f98129c3b60fe00bb90
strykejernene <strykejernene@32353942-264d-11df-90c7-81228a7a91d5>
2010-03-08 22:58:19 +0000

    Spacehip steering with mouse fixed

--- trunk/src/gameEngine/Input.java
+++ trunk/src/gameEngine/Input.java
@@ -1,15 +1,19 @@
 package gameEngine;
 
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 
-public class Keyboard {
+public class Input {
 	public boolean up, down, left, right;
+	public Vector mousePos;
 	
-	public Keyboard(){
+	public Input(){
 		up = false;
 		down = false;
 		left = false;
 		right = false;
+		mousePos = new Vector();
 	}
 	
 	public void update(KeyEvent key, boolean pressed){
@@ -17,5 +21,15 @@ public class Keyboard {
 		else if (key.getKeyCode() == KeyEvent.VK_DOWN) down = pressed;
 		else if (key.getKeyCode() == KeyEvent.VK_LEFT) left = pressed;
 		else if (key.getKeyCode() == KeyEvent.VK_RIGHT) right = pressed;
+	}
+	
+	public void update(int x, int y){
+		mousePos.x = x-5;
+		mousePos.y = y-25;
+	}
+	
+	public void drawCrosshair(Graphics g){
+		g.setColor(Color.green);
+		g.drawOval(mousePos.getX()-5, mousePos.getY()-5, 10, 10);
 	}
 }
