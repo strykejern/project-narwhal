0264ee154a9778da12dbdcaa549f856e9275c678
jnsn.johan <jnsn.johan@32353942-264d-11df-90c7-81228a7a91d5>
2010-03-10 00:58:51 +0000

    Fixed possible bug. Made the logging module more cross platform.

--- trunk/src/gameEngine/Log.java
+++ trunk/src/gameEngine/Log.java
@@ -19,7 +19,6 @@ package gameEngine;
 //********************************************************************************************
 
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Calendar;
@@ -29,7 +28,10 @@ public class Log
 {
 	static BufferedWriter logFile = null;
     
-	//JJ> Initialize the logging system
+	/**
+	 * JJ> Initialize the logging system
+	 * @return true if it was successful.
+	 */
 	public static boolean initialize() {
 	     try 
 	     {	            
@@ -41,42 +43,56 @@ public class Log
 	          message("Initializing logging system... Success!");
 	          return true;
 	      } 
-	      catch (FileNotFoundException ex) 
+	      catch (Exception ex) 
 	      {
 	          ex.printStackTrace();
 	      }   
-	      catch (IOException ex) 
-	      {
-	          ex.printStackTrace();
-	      } 
-	        
+	   
+	   //Something went wrong
 	   return false;
 	}
 		
 	
-	//JJ> Writes the specified text to the log file and adds a new line feed	
+	/**
+	 * JJ> Writes the specified text to the log file and adds a new line feed	
+	 * @param rawText
+	 */
 	public static void message(String rawText) {
-		//logFile.newLine();		//@TODO: JJ> should use this function instead of \n
-	 	//    							since new line feeds are system dependent
-		String text = "INFO: " + rawText + "\n"; 
-		System.out.print(text);
-		print(text);
-	}
-	public static void warning(String rawText) {
-		String text = "WARNING: " + rawText + "\n"; 
-		System.err.print(text);
-		print(text);
-	}
-	public static void error(String rawText) {
-		String text = "FATAL ERROR: " + rawText + "\n"; 
-		System.err.print(text);
-		close();
-		print(text);
-		System.exit(-1);
+		String text = "INFO: " + rawText; 
+		System.out.println(text);
+		println(text);
 	}
 	
-	//JJ> Writes the specified text to the log file
-	public static void print(String text) {
+	/**
+	 * JJ> Writes the specified warning to the log file and adds a new line feed	
+	 * @param rawText
+	 */
+	public static void warning(String rawText) {
+		String text = "WARNING: " + rawText; 
+		System.err.println(text);
+		println(text);
+	}
+	public static void warning(Exception ex) { warning(ex.toString()); }
+	
+	/**
+	 * JJ> Writes the specified error to the log file and then terminates the program	
+	 * @param rawText
+	 */
+	public static void error(String rawText) {
+		String text = "FATAL ERROR: " + rawText; 
+		System.err.println(text);
+		println(text);
+		close();
+		System.exit(-1);
+	}
+	public static void error(Exception ex) { error(ex.toString()); }
+	
+	/**
+	 * JJ> Writes the specified text to the log file
+	 * @param text
+	 */
+	public static void println(String text) { print(text, true); }
+	public static void print(String text, boolean newLine) {
 		if( logFile == null ) return;
 		
 		String message = getTime() + " " + text;
@@ -85,14 +101,14 @@ public class Log
 	        //Writing happens live, we want the log to be up to date if the 
 	        //program crashes for some reason
 	        logFile.write(message);
+			if(newLine) logFile.newLine(); 	//We use this function instead of \n, since new line feeds are system dependent
 	        logFile.flush();	        
 	     } 	       
-	     catch (FileNotFoundException ex) {
+	     catch (Exception ex) 
+	     {
 	    	 ex.printStackTrace();
 	     } 	        
-	     catch (IOException ex) {
-	    	 ex.printStackTrace();
-	     } 
+
 	}
 	
 	//JJ> Helper functions to quickly get time and date
@@ -107,7 +123,9 @@ public class Log
     	return date.format( cal.getTime() );
     }
 	
-	//JJ> Close log file
+	/**
+	 * JJ> Close log file
+	 */
     public static void close() {
 		if (logFile == null) return; 
 
