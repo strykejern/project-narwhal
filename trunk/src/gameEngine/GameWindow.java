3560c3b1c7662fad94b14a1d2aa95be403d4b709
jnsn.johan <jnsn.johan@32353942-264d-11df-90c7-81228a7a91d5>
2010-03-18 01:55:15 +0000

    Configuration settings are now saved and stored in a ini file.

--- trunk/src/gameEngine/GameWindow.java
+++ trunk/src/gameEngine/GameWindow.java
@@ -26,6 +26,14 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -63,12 +71,10 @@ public class GameWindow extends JPanel implements Runnable, KeyListener, MouseLi
     	//Initialize the logging system, do this first so that error logging happens correctly.
     	Log.initialize();
     	
-		//Now initialize Video settings
+    	//Load settings
         Video.initialize();
-        Video.setVideoQuality( VideoQuality.VIDEO_NORMAL );
-        Video.setResolution(800, 600);
-//        Video.setFullscreen();
-
+    	loadSettings();
+    	
 		//Initialize the frame window where we draw stuff
     	JFrame parentWindow = new JFrame("Project Narwhal", Video.getGraphicsConf());		
     	parentWindow.getContentPane().add(new GameWindow(parentWindow));
@@ -108,10 +114,7 @@ public class GameWindow extends JPanel implements Runnable, KeyListener, MouseLi
     	
 		// Set the blank cursor to the JFrame.
 		//frame.getContentPane().setCursor(Video.blankCursor);
-		
-    	//Load game resources TODO: Move this somewhere else
-		//Sound crash = new Sound("data/crash.au");
- 	
+		 	
 		while( state != gameState.GAME_EXIT )
     	{
 			//Update mouse position within the frame
@@ -145,10 +148,153 @@ public class GameWindow extends JPanel implements Runnable, KeyListener, MouseLi
 	}
 	
 	/**
+	 * JJ> Load configuration settings
+	 */
+	private static void loadSettings() {
+		File conf = new File("config.ini");
+		boolean useDefault = false;
+		
+		//See if a configuration file exists
+		if( conf.exists() )
+		{
+			try 
+			{
+				int resX = 800, resY = 600;
+				BufferedReader parse = new BufferedReader( new FileReader(conf) );
+				
+				//Parse the config file
+				while(true)
+				{
+					String line = parse.readLine();
+					
+					//Reached end of file
+					if(line == null) break;
+					
+					//Graphic quality
+					if(line.startsWith("[GRAPHICS]:"))
+					{
+						if( line.endsWith("HIGH") ) Video.setVideoQuality(VideoQuality.VIDEO_HIGH);
+						else if( line.endsWith("LOW") ) Video.setVideoQuality(VideoQuality.VIDEO_LOW);
+						else Video.setVideoQuality(VideoQuality.VIDEO_NORMAL);
+					}
+					
+					//Sound enabled
+					else if(line.startsWith("[SOUND]:"))
+					{
+						if( line.endsWith("OFF") ) Sound.enabled = false;
+						else Sound.enabled = true;
+					}
+					
+					//Full screen
+					else if(line.startsWith("[FULL_SCREEN]:"))
+					{
+						if( line.endsWith("TRUE") ) Video.setFullscreen();
+					}
+					
+					//Resolution (overrides full screen
+					else if(line.startsWith("[SCREEN_WIDTH]:"))
+					{
+						line = line.substring(line.indexOf(":")+1);
+						resX = Integer.parseInt(line.trim());
+					}
+					else if(line.startsWith("[SCREEN_HEIGHT]:"))
+					{
+						line = line.substring(line.indexOf(":")+1);
+						resY = Integer.parseInt(line.trim());
+					}
+				}
+				
+				//Set screen dimensions
+				Video.setResolution(resX, resY);
+				
+				//Close file
+				Log.message("Configuration file successfully parsed.");
+				parse.close();
+			} 
+			
+			//Something went wrong, revert to default settings
+			catch (Exception e) 
+			{
+				Log.warning(e);
+				useDefault = true;
+			}
+
+		}
+		else useDefault = true;
+		
+		//Use default settings
+		if(useDefault)
+		{
+			Log.message("Could not read configuration settings. Reverting to default settings.");
+			Video.setVideoQuality(VideoQuality.VIDEO_NORMAL);
+			Sound.enabled = true;
+	        Video.setResolution(800, 600);
+		}
+	}
+	
+	/**
+	 * JJ> Save current configuration settings
+	 */
+	private void exportSettings(){
+		File conf = new File("config.ini");
+		
+		//Delete any existing file
+		conf.delete();
+		if( !conf.exists() )
+		{
+			try 
+			{
+				conf.createNewFile();
+				BufferedWriter save = new BufferedWriter( new FileWriter(conf) );
+				
+				//Graphic quality
+				save.write( "[GRAPHICS]: " );
+				if( Video.getQualityMode() == VideoQuality.VIDEO_LOW ) save.write("LOW");
+				else if( Video.getQualityMode() == VideoQuality.VIDEO_HIGH ) save.write("HIGH");
+				else save.write("NORMAL");
+				save.newLine();
+				
+					
+				//Sound enabled
+				save.write( "[SOUND]: " );
+				if( Sound.enabled ) save.write("ON");
+				else save.write("OFF");
+				save.newLine();
+					
+				//Full screen
+				save.write("[FULL_SCREEN]: ");
+				if (Video.getDesktopResolution() == Video.getResolution())
+					save.write("TRUE");
+				else save.write("FALSE");
+				save.newLine();
+					
+				//Resolution (overrides full screen
+				save.write("[SCREEN_WIDTH]: ");
+				save.write( "" + Video.getScreenWidth() );
+				save.newLine();
+				save.write("[SCREEN_HEIGHT]: ");
+				save.write( "" + Video.getScreenHeight() );
+				save.newLine();
+				
+				save.close();
+				Log.message("Configuration settings saved: " + conf.getAbsolutePath() );
+			} 
+			
+			//Something went wrong
+			catch (Exception e) 
+			{
+				Log.warning("Could not save settings: " + e);
+			}
+		}
+		else Log.warning("Could not delete old config file.");
+	}
+	
+	/**
 	 * JJ> Free resources, save data and exit properly
 	 * @param code The exit code used for terminating this process (0 for normal exit)
 	 */
 	private void exit(int code) {
+		exportSettings();
 		Log.message("Exiting the game the good way. Exit code: " + code);
 	   	Log.close();		
 	   	System.exit(code);
