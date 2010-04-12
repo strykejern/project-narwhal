9b9de2f4d8a5424d07898113ffd044514a9df7ac
jnsn.johan <jnsn.johan@32353942-264d-11df-90c7-81228a7a91d5>
2010-04-12 23:31:06 +0000

    Smarter AI

--- src/narwhal/AI.java
+++ src/narwhal/AI.java
@@ -3,6 +3,18 @@ package narwhal;
 import gameEngine.Input;
 import gameEngine.Vector;
 
+/**
+ * JJ> This implements the game artificial intelligence using "FSM" logic. 
+ * "Finite State Machine" logic uses a simple finite state machine to make its decisions. 
+ * By reacting to events that have occurred, it can take on a personality that is almost lifelike:
+ * 1. Initially, the AI will try to chase the target.
+ * 2. Then it retreats, possibly since it took some damage during the confrontation.
+ * 3. When far enough away, it moves about in a pattern to regain its compose.
+ * 4. Then, after circling the target a bit to see what's going on, it begins its chase again (state 1).
+ * Essentially, the "FSM" system is an amalgamation of different AI states.
+ * @author Anders Eie and Johan Jansen
+ *
+ */
 public class AI extends Spaceship {
 	private Spaceship target;
 	private aiLevel   level;
@@ -13,9 +25,9 @@ public class AI extends Spaceship {
 	//Ambush - Attacks from behind, retreats if shot on
 	//Controller - Tries to adapt tactics and use special powers
 	enum aiLevel{
-		STUPID,				//Extra stupid AI, no pathfinding?
+		STUPID,				//Extra stupid AI, no pathfinding? Do not avoid planets or black holes?
 		DEFAULT,			//The normal AI
-		SMART				//Perfect calculations? Tries to attack player from behind?
+		SMART				//Perfect calculations? Tries to attack player from behind? Dodge enemy fire?
 	}
 	
 	enum aiState {
@@ -31,7 +43,7 @@ public class AI extends Spaceship {
 		keys.setCameraPos(new Vector());
 		target = this;
 		state = aiState.INTERCEPT;
-		level = aiLevel.STUPID;
+		level = aiLevel.DEFAULT;
 	}
 	
 	public void setTarget(Spaceship target) {
@@ -49,7 +61,20 @@ public class AI extends Spaceship {
 		//Calculate distance from target
 		Vector vDistance = target.getPosCentre().minus(getPosCentre());
 		float fDistance = Math.abs(vDistance.x + vDistance.y);
-
+		
+		//Consider retreating if we are not stupid
+		if( level != aiLevel.STUPID )
+		{
+			//Retreat if shields are down
+			//but only if the enemy has some shields left and some energy to spend
+			if( ( shieldMax != 0 && shield == 0 ) && target.shield != 0 
+					&& target.energy > target.energyMax/10 ) 			state = aiState.RETREAT;
+	
+			//Retreat if less than 10% energy left
+			//but only if our enemy has more energy than us (cheat!)
+			else if( energy < energyMax/10 && target.energy > energy ) state = aiState.RETREAT;
+		}
+		
 		//AI State - Intercept
 		if( state == aiState.INTERCEPT )
 		{
@@ -88,6 +113,27 @@ public class AI extends Spaceship {
 			//- if under attack from behind
 			//- need to regenerate energy
 			//- to prepare an ambush
+			
+			//Stop retreating if we are ready (33% energy and 20% shield)
+			if( energy > energyMax/3 && shield > shieldMax/5) state = aiState.INTERCEPT;
+			
+			//Run away
+			if(fDistance < 500)
+			{
+				keys.up = true;
+				keys.down = false;
+			}
+			else
+			{
+				keys.up = false;
+				keys.down = true;
+			}
+			keys.mosButton1 = false;
+			
+			//Opposite direction of target
+			//not exactly, but it will do for now
+			keys.mousePos = target.getPosCentre().clone();
+			keys.mousePos.rotateTo(2-keys.mousePos.getAngle());
 		}
 		
 		super.update();
